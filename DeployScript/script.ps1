1acc11adf3d7e1e5ce3201751fd02296820fc73d
Serhii Basetskyi <sbs@scalepoint.com>
2025-02-28 20:46:32 +0100

    8

--- DeployScript/script.ps1
+++ DeployScript/script.ps1
@@ -7,5 +7,5 @@ param (
 
 Write-Output "Param1: $Param1"
 Write-Output "Param2: $Param2"
-Write-Output "Param2: $Param3"
-Write-Output "Param2: $Param4"
+Write-Output "Param3: $Param3"
+Write-Output "Param4: $Param4"
